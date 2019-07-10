d82d56c1b97f29163d5fded7abb44999d0f537b6
Germán Arevalo <germanarevalojerez@gmail.com>
2019-07-10 11:51:18 -0500

    Calculating scores with distance and game time

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -7,10 +7,13 @@ public class GameView : MonoBehaviour
 {
     public Text coinsText, scoreText, maxScoreText;
 
+    PlayerController controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GameObject.Find("Player")
+            .GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -19,8 +22,8 @@ public class GameView : MonoBehaviour
         if (GameManager.sharedInstance.currentGameState == GameState.inGame)
         {
             int coins = GameManager.sharedInstance.collectedObject;
-            float score = 0;
-            float maxScore = 0;
+            float score = controller.GetTravelledDistance();
+            float maxScore = PlayerPrefs.GetFloat("maxscore", 0f);
 
             coinsText.text = coins.ToString();
             scoreText.text = "Score: " + score.ToString("f1");
